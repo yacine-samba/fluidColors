2684cab242a5c7b592fdf9c1b4ad2a96a81ebd2a
Yacine Samba <ysambamezaache@gmail.com>
2023-03-27 01:37:13 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test FluidColors in ThreeJS
---
+++ src/shaders/fragment.js
@@ -0,0 +1,14 @@
+export default /*glsl*/ `
+
+varying vec3 vPosition;
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vPattern;
+
+uniform float uTime;
+
+void main() {
+  float pattern = vPosition.x;
+  gl_FragColor = vec4(vec3(0.2 , 0.4, 1.0) , 1.0);
+}
+`;
